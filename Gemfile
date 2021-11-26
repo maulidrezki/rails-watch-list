f8216f0768bd516d89a67f3fb95969c27a2f134c
Maulid Rezki <maulidrezki13@gmail.com>
2021-11-26 12:35:22 +0700

    editing

--- Gemfile
+++ Gemfile
@@ -55,4 +55,3 @@ gem 'font-awesome-sass', '~> 5.12.0'
 gem 'simple_form'
 gem 'dotenv-rails', groups: [:development, :test]
 gem 'cloudinary', '~> 1.16.0'
-
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,6 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-import 'bootstrap';
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
@@ -17,3 +16,5 @@ document.addEventListener('turbolinks:load', () => {
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+import 'bootstrap'
